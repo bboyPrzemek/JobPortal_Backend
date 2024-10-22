2ebfe80d0090ee4fe0bb5558ca58681c996af639
Przemyslaw Rogala <przemorog01@gmail.com>
2024-10-22 18:47:24 +0200

    Update DemoApplication.java
    removed comments

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,13 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) throws MalformedURLException, URISyntaxException {
-//		System.out.println(System.getProperty("java.runtime.version"));
-//		System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
-//		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
-//		System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
-//		System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
-//		System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dumpTreshold", "99999");
-		
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
